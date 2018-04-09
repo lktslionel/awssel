287b4886b84579348716c8e7633d1a60a052b3f2
lktslionel <lktslionel@gmail.com>
2018-04-09 15:32:35 +0200

    code(cli): Init cobra app

---
+++ cmd/root.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "awssel",
+	Short: "Load service env vars from AWS SSM Parameter Store",
+	Long:  `A tool that load all env vars stored in AWS SSM Parameter Store, for a given service.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	// Put init logic here
+}
