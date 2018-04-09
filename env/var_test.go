f6da03d4d257de82c2467a60100b212125461eca
lktslionel <lktslionel@gmail.com>
2018-04-09 18:20:24 +0200

    test(env/var): Format env var with the bashExportFormatter -- passed

--- env/var_test.go
+++ env/var_test.go
@@ -1,12 +1,30 @@
 package env
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	examples = []struct {
+		ev       Var
+		expected string
+	}{
+		{Var{"PROXY_APP_DIR", "/os/app"}, "'PROXY_APP_DIR=/os/app'"},
+		{Var{"API_ID", "0ac41e50-3364-11e8-9d7e"}, "'API_ID=0ac41e50-3364-11e8-9d7e'"},
+		// Escape some special characters: [:space:], $
+		{Var{"PROXY_USERNAME", "os operator"}, "'PROXY_USERNAME=os operator'"},
+		{Var{"PROXY_PASS", `784"f43631c05`}, `'PROXY_PASS=784"f43631c05'`},
+		{Var{"KEY_STRING", `cURmOUl4U VVX"$ESCAPE"ZHRsdWs=`}, `'KEY_STRING=cURmOUl4U VVX"$ESCAPE"ZHRsdWs='`},
+	}
+
+	// Formatter
+	fmtr Formatter
 )
 
 func TestVar(t *testing.T) {
@@ -29,23 +47,8 @@ func TestVar(t *testing.T) {
 	})
 
 	t.Run("Export Var using DefaultFormatter", func(t *testing.T) {
-		var fmtr Formatter
-		//{DefaultFormatter(), "HOME=/home/username"},
-		//{BashExportFormatter(), "export HOME=/home/username"},
 
-		examples := []struct {
-			ev       Var
-			expected string
-		}{
-			{Var{"PROXY_APP_DIR", "/os/app"}, "'PROXY_APP_DIR=/os/app'"},
-			{Var{"API_ID", "0ac41e50-3364-11e8-9d7e"}, "'API_ID=0ac41e50-3364-11e8-9d7e'"},
-			// Escape some special characters: [:space:], $
-			{Var{"PROXY_USERNAME", "os operator"}, "'PROXY_USERNAME=os operator'"},
-			{Var{"PROXY_PASS", `784"f43631c05`}, `'PROXY_PASS=784"f43631c05'`},
-			{Var{"KEY_STRING", `cURmOUl4U VVX"$ESCAPE"ZHRsdWs=`}, `'KEY_STRING=cURmOUl4U VVX"$ESCAPE"ZHRsdWs='`},
-		}
-
-		t.Run("Using DefaultFormatter", func(t *testing.T) {
+		t.Run("It should output env var as k=v", func(t *testing.T) {
 			fmtr = DefaultFormatter()
 
 			for _, ex := range examples {
@@ -55,8 +58,20 @@ func TestVar(t *testing.T) {
 
 		})
 
-		t.Run("Using BashExportFormatter", func(t *testing.T) {
-			t.Skip()
+	})
+
+	t.Run("Export Var using BashExportFormatter", func(t *testing.T) {
+		var fmtr Formatter
+
+		t.Run("It should output env var as k=v", func(t *testing.T) {
+			fmtr = BashExportFormatter()
+
+			for _, ex := range examples {
+				actual := ex.ev.Export(fmtr)
+				assert.Equal(t, actual, fmt.Sprintf("export %s", ex.expected))
+			}
+
 		})
+
 	})
 }
