b0f63e4cf140487525e0106459e7cdc1030708fb
lktslionel <lktslionel@gmail.com>
2018-04-12 18:31:12 +0200

    code(cmd/load): init CLI UI

--- cmd/load.go
+++ cmd/load.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/lktslionel/awssel/env"
 	"github.com/spf13/cobra"
 )
 
@@ -10,23 +11,53 @@ import (
 var loadCmd = &cobra.Command{
 	Use:   "load",
 	Short: "Load env vars for a service",
-	Long:  `Load all env vars stored in SSM Parameter Store for a given service`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("load called")
+	Long: `Load all environment variables stored in 
+AWS SSM Parameter Store for a given service`,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
 
+		fmt.Println(args)
+		fmt.Println("serviceName", serviceName)
+		fmt.Println("prefixPath", prefixPath)
+		fmt.Println("awsRegion", awsRegion)
+		fmt.Println("filterPattern", filterPattern)
+		fmt.Println("outputFormat", outputFormat)
+		fmt.Println("exportable", exportable)
+		fmt.Println("awsRole", awsRole)
+		fmt.Println("awsProfile", awsProfile)
+
+		return
 	},
 }
+
+// Flags variables
+var (
+	serviceName   string
+	prefixPath    string
+	awsRegion     string
+	filterPattern string
+	outputFormat  string
+	exportable    bool
+	awsRole       string
+	awsProfile    string
+)
 
 func init() {
 	rootCmd.AddCommand(loadCmd)
 
-	// Here you will define your flags and configuration settings.
+	// Flags
+	//loadCmd.Flags().BoolP("help", "h", true, "Show help")
+	loadCmd.Flags().StringVar(&serviceName, "service-name", "", "service name (required)")
+	loadCmd.Flags().StringVar(&prefixPath, "prefix-path", "", "Prefix output string with 'export' statement (required)")
+	loadCmd.Flags().StringVar(&awsRegion, "aws-region", "", "AWS region name (required)")
+	loadCmd.Flags().StringVar(&filterPattern, "filter-pattern", "", "regex that every env var name must match")
+	loadCmd.Flags().StringVar(&outputFormat, "output-format", env.DefaultFormat, "env vars's output format")
+	loadCmd.Flags().BoolVar(&exportable, "export", false, "Prefix output string with export statement")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// loadCmd.PersistentFlags().String("foo", "", "A help for foo")
+	loadCmd.MarkFlagRequired("service-name")
+	loadCmd.MarkFlagRequired("prefix-path")
+	loadCmd.MarkFlagRequired("aws-region")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// loadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	loadCmd.Flags().SortFlags = false
+	//loadCmd.Flags().StringVar(&awsRole, 			"awsRole"
+	//loadCmd.Flags().StringVar(&awsProfile, 		"awsProfile"
 }
--- docs/awssel/cmd/load.md
+++ docs/awssel/cmd/load.md
@@ -18,6 +18,6 @@ Name &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp
  `--aws-region` | AWS Region Name | Yes | -
  `--filter-pattern` | The pattern that every env var name must match | No | To get all var names prefixed starting with `OS_`,<br>use this filter pattern `--filter-pattern=OS_*`
  `--output-format` | Output env vars to : `default, json, yaml` format | No | 
- `--exportable` | Prefix output string with `export` statement.<br>It only works with `output-format` set `default` | No | Output `export NAME=VALUE`
+ `--export` | Prefix output string with `export` statement.<br>It only works with `output-format` set `default` | No | Output `export NAME=VALUE`
  `--aws-role` | AWS Role ARN. <br>Note that the role must have a right set of permission to actually request anything for AWS parameter store | No |`os-fake-role`
  `--aws-profile` | AWS Profile Name | No | -
